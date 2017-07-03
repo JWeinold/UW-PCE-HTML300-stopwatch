ed137d6242baca10378adc1718458a6e591cd70d
Jane Weinold <JWeinold@perkinscoie.com>
2017-07-02 23:10:18 -0700

    second attempt

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,19 @@
+// button controls
+const start = document.querySelector('button.start')
+const stop = document.querySelector('button.stop')
+
+// DOM elements that I need to update
+const lapList = document.querySelector('#lapList')
+const stopwatchTime = document.querySelector('#stopwatchTime')
+
+// constants that shouldn't ever change
+const laps = []
+const intervalRate = 10 // update the stopwatch every 10 milliseconds
+
+// values that will change pretty often
+let intervalId = null
+let rawTime = 0
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -8,6 +24,32 @@ function formatTime (raw) {
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
+// start the stopwatch by creating a new interval
+// we'll store the interval id so we can manipulate the interval later
+function stopwatchStart (event) {
+  event.preventDefault()
+  console.log('started!')
+
+  // every 10 milliseconds, update the stopwatch
+  // Didn't start this with var or let on the next line because it is already defined at the top -- at the top so every function can see it
+  intervalId = setInterval(stopwatchUpdate, intervalRate)
+}
+
+// adds the interval to the stopwatch time since the last "tick"
+// then update the DOM with the new stopwatch time
+function stopwatchUpdate () {
+  rawTime += intervalRate
+  stopwatchTime.innerHTML = formatTime(rawTime)
+}
+
+// stops the stopwatch by clearing the interval
+function stopwatchStop (event) {
+  event.preventDefault()
+  console.log('stopped!')
+
+  clearInterval(intervalId)
+}
+
 // adds a leading zero because humans like them
 function zeroPad (value) {
   let pad = value < 10 ? '0' : ''
@@ -16,4 +58,8 @@ function zeroPad (value) {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+
+
+  start.addEventListener("click", stopwatchStart)
+  stop.addEventListener("click", stopwatchStop)
 })
